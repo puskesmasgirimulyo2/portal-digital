8482399aa7f37d708d6dd0b79dbce093fdee0e46
puskesmasgirimulyo2 <puskgirimulyo2cerdas@gmail.com>
2026-09-14 14:33:42 +0700

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,25 @@
+{
+  "name": "e-Portal Digital Puskesmas Girimulyo II",
+  "short_name": "e-Portal G2",
+  "description": "Portal Terintegrasi Layanan Digital Puskesmas Girimulyo II",
+  "start_url": "./",
+  "scope": "./",
+  "display": "standalone",
+  "orientation": "portrait-primary",
+  "background_color": "#1A1210",
+  "theme_color": "#B3261E",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
